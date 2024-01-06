8982406aff729c559a49400d17b91ea7eb262791
Alex Huang <huangalex409@gmail.com>
2024-01-06 18:06:48 -0500

    refactor kernel.c and boot.s
    boot.s now calls terminal_init, and calls the kernel_real and kernel_main functions. kernel_real performs real mode init and other important things.
    kernel_main is the real kernel.

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/iasm.h>
 
+/**
+ * Kernel C code that expects to be run in real mode, do it before we switch to
+ * protected in kernel_main
+ */
+void kernel_real(void) {
+    printf("Hello, real mode kernel World!\n");
+}
+
+/**
+ * Actual main entrypoint of the kernel, this will be run in protected so make
+ * sure all BIOS calls and other important real mode operations is performed in
+ * kernel_real before control is transfered here by boot.S
+ */
 void kernel_main(void) {
-	terminal_initialize();
-	printf("Hello, kernel World!\n");
+    printf("Hello, protected mode kernel World!\n");
+
+    kpanic("Kernel shutdown.");
 }
