460041112af400795fa1594c1c88704729bcf59a
Alex Huang <huangalex409@gmail.com>
2024-01-07 09:14:44 -0500

    Add copyright header to files.

--- kernel/include/kernel/iasm.h
+++ kernel/include/kernel/iasm.h
@@ -1,3 +1,10 @@
+/* Copyright (C) 2024 Alex Huang - All Rights Reserved
+ * You may use, distribute and modify this code under the terms of the GPL license.
+ *
+ * File is part of the BrowserOS research project, all bug reports should be
+ * submitted to teapot4195[at]gmail[dot]com.
+ */
+
 #ifndef _KERNEL_IASM_H
 #define _KERNEL_IASM_H
 
--- kernel/kernel/kpanic.c
+++ kernel/kernel/kpanic.c
@@ -1,3 +1,10 @@
+/* Copyright (C) 2024 Alex Huang - All Rights Reserved
+ * You may use, distribute and modify this code under the terms of the GPL license.
+ *
+ * File is part of the BrowserOS research project, all bug reports should be
+ * submitted to teapot4195[at]gmail[dot]com.
+ */
+
 #include <string.h>
 
 #include <kernel/tty.h>
